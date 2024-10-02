08cc324c9d4e91355dd5ae0f2cdac9133de91a87
SifaKasena <109460248+SifaKasena@users.noreply.github.com>
2024-10-02 23:14:14 +0300

    [Fix:] Settle promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
 
-  return Promise.all([user, photo]);
+  return Promise.allSettled([user, photo])
+    .then((values) => values);
 }
