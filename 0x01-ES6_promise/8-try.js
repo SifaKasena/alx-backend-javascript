1b3af161d92350070b6a37e8371058c717d004a3
SifaKasena <109460248+SifaKasena@users.noreply.github.com>
2024-10-02 11:26:23 +0300

    Try

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,11 @@
 export default function divideFunction(numerator, denominator) {
   try {
-    if (denominator === 0) {
+    if (denominator !== 0) {
+      return numerator / denominator;
+    }
+    else {
       throw new Error('cannot divide by 0');
     }
-    return numerator / denominator;
   } catch (error) {
     return error;
   }
